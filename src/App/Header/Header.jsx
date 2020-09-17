42ab2f706a800ab345ed2b9178bd596264de58af
Delphine GRANDJEAN <delf.grandjean@orange.fr>
2020-09-17 14:01:48 +0200

    fix(header): modification texte h1

--- src/App/Header/Header.jsx
+++ src/App/Header/Header.jsx
@@ -4,7 +4,7 @@ import './Header.css';
 function Header() {
   return (
     <header className="Header">
-      <h1>Super Calculateur de nombre premier</h1>
+      <h1>Super Calculateur de nombre premier.</h1>
     </header>
   );
 }
