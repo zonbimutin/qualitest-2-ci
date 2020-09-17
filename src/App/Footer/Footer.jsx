f03d3bd0783583dbe077ac3ec2762d7aab420daa
Felipe Alarcon <zonbimutin@gmail.com>
2020-09-17 13:50:20 +0200

    update(footer) // Add Felipe

--- src/App/Footer/Footer.jsx
+++ src/App/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Footer.css';
 
 function Footer() {
-  return <footer className="Footer">© 2020 - Tous droits réservés</footer>;
+  return <footer className="Footer">© 2020 - Tous droits réservés à Felipe Alarcon</footer>;
 }
 
 export default Footer;
