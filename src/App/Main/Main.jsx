a260a30cc848adcb9c191a43bf949ab44b4a30f4
Felipe Alarcon <zonbimutin@gmail.com>
2020-09-17 15:44:30 +0200

    fix(ci) : error ci esllint main

--- src/App/Main/Main.jsx
+++ src/App/Main/Main.jsx
@@ -4,7 +4,6 @@ import IsPrime from './IsPrime/IsPrime';
 import './Main.css';
 
 function Main() {
-  const error = 0;
   /**
    * Sauvegarde locale du nombre à tester
    */
@@ -12,7 +11,7 @@ function Main() {
 
   return (
     <main className="Main">
-      <p>Entrez un nombre, nous allons vérifier si il sagit d'un nombre premier</p>
+      <p>Entrez un nombre, nous allons vérifier si il sagit dun nombre premier</p>
       <NumberInput setNumber={setNumber} />
       <IsPrime number={number} />
     </main>
